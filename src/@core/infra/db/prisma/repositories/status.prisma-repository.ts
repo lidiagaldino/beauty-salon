cee1b5f5f1eae506a1b696beaadce088f4ae28c7
lidiagaldino <lidiagabrielly14@gmail.com>
2024-02-12 20:38:03 -0300

    set status id

--- src/@core/infra/db/prisma/repositories/status.prisma-repository.ts
+++ src/@core/infra/db/prisma/repositories/status.prisma-repository.ts
@@ -9,24 +9,36 @@ export class StatusPrismaRepository implements IStatusRepository {
         status: status.getName(),
       },
     });
-    return Status.create({ name: result.status });
+    const createdStatus = Status.create({ name: result.status });
+    createdStatus.setId(result.id);
+    return createdStatus;
   }
   async findAll(): Promise<Status[]> {
     const result = await prisma.tbl_status.findMany();
-    return result.map((status) => Status.create({ name: status.status }));
+    const status = result.map((item) => {
+      const status = Status.create({ name: item.status });
+      status.setId(item.id);
+      return status;
+    });
+
+    return status;
   }
   async findById(id: number): Promise<Status> {
     const result = await prisma.tbl_status.findUnique({
       where: { id },
     });
-    return Status.create({ name: result.status });
+    const status = Status.create({ name: result.status });
+    status.setId(result.id);
+    return status;
   }
   async update(status: Status): Promise<Status> {
     const result = await prisma.tbl_status.update({
       where: { id: status.getId() },
       data: { status: status.getName() },
     });
-    return Status.create({ name: result.status });
+    const updatedStatus = Status.create({ name: result.status });
+    updatedStatus.setId(result.id);
+    return updatedStatus;
   }
   async delete(id: number): Promise<void> {
     await prisma.tbl_status.delete({
