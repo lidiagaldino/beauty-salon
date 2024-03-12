9182005de6e8b46ceb8245d4bf29522f62f47a20
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-12 17:10:12 -0300

    change phone fields to string type

--- src/@core/domain/value-objects/phone.value-object.ts
+++ src/@core/domain/value-objects/phone.value-object.ts
@@ -1,7 +1,7 @@
 export type TPhoneProps = {
-  number: number;
-  ddi: number;
-  ddd: number;
+  number: string;
+  ddi: string;
+  ddd: string;
 };
 
 export class Phone {
@@ -14,27 +14,39 @@ export class Phone {
   public static create(props: TPhoneProps): Phone {
     return new Phone(props);
   }
+  public static createFromString(phone: string): Phone {
+    const [ddd, ddi, number] = phone.split('');
+    return Phone.create({
+      ddd: ddd,
+      ddi: ddi,
+      number: number,
+    });
+  }
 
-  get number(): number {
+  get number(): string {
     return this.props.number;
   }
-  get ddi(): number {
+  get ddi(): string {
     return this.props.ddi;
   }
-  get ddd(): number {
+  get ddd(): string {
     return this.props.ddd;
   }
-  set number(value: number) {
+  set number(value: string) {
     this.props.number = value;
   }
-  set ddi(value: number) {
+  set ddi(value: string) {
     this.props.ddi = value;
   }
-  set ddd(value: number) {
+  set ddd(value: string) {
     this.props.ddd = value;
   }
   public toString(): string {
     return JSON.stringify(this.props);
+  }
+
+  public toStringFormat(): string {
+    return `+${this.ddi} (${this.ddd}) ${this.number}`;
   }
   public toJSON(): TPhoneProps {
     return this.props;
