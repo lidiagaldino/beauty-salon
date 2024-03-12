8eed6c5ea99f2f34e64b9e279ec52646140bf3ea
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-12 19:51:11 -0300

    add isAdmin and isClient methods to validate user type

--- src/@core/domain/interfaces/user-cryptography.interface.ts
+++ src/@core/domain/interfaces/user-cryptography.interface.ts
@@ -3,4 +3,6 @@ import { IUserPayload } from './user-payload.interface';
 export interface IUserCryptography {
   verify(value: string): IUserPayload;
   encrypt(object: IUserPayload): string;
+  isClient(value: string): boolean;
+  isAdmin(value: string): boolean;
 }
