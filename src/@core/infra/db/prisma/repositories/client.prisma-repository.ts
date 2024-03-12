6b500d7bd9d03f56c7d46a89baab44c1de61e0c5
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-12 17:23:04 -0300

    add client repository implementation

---
+++ src/@core/infra/db/prisma/repositories/client.prisma-repository.ts
@@ -0,0 +1,90 @@
+import { prisma } from '..';
+import { Client } from '../../../../domain/entities/client.entity';
+import { IClientRepository } from '../../../../domain/repositories/client.repository';
+import { Email } from '../../../../domain/value-objects/email.value-object';
+import { Phone } from '../../../../domain/value-objects/phone.value-object';
+
+export class ClientPrismaRepository implements IClientRepository {
+  async create(client: Client): Promise<Client> {
+    const result = await prisma.tbl_client.create({
+      data: {
+        name: client.getName(),
+        login: client.getLogin().email,
+        password: client.getPassword(),
+        phone: client.getPhone().toStringFormat(),
+      },
+    });
+
+    client.setId(result.id);
+    return client;
+  }
+  async findAll(): Promise<Client[]> {
+    const result = await prisma.tbl_client.findMany();
+    const clients = result.map((item) => {
+      const client = Client.create({
+        name: item.name,
+        login: Email.create({ email: item.login }),
+        phone: Phone.create({
+          ddd: item.phone,
+          ddi: item.phone,
+          number: item.phone,
+        }),
+      });
+      client.setId(item.id);
+      return client;
+    });
+    return clients;
+  }
+  async findById(id: number): Promise<Client> {
+    const result = await prisma.tbl_client.findUnique({
+      where: { id },
+    });
+    if (!result) return null;
+    const client = Client.create({
+      name: result.name,
+      login: Email.create({ email: result.login }),
+      phone: Phone.create({
+        ddd: result.phone,
+        ddi: result.phone,
+        number: result.phone,
+      }),
+    });
+    client.setId(result.id);
+    return client;
+  }
+  async update(client: Client): Promise<Client> {
+    const result = await prisma.tbl_client.update({
+      where: { id: client.getId() },
+      data: {
+        name: client.getName(),
+        login: client.getLogin().email,
+        phone: client.getPhone().toStringFormat(),
+      },
+    });
+    client.setId(result.id);
+    return client;
+  }
+  async delete(id: number): Promise<void> {
+    await prisma.tbl_client.delete({
+      where: { id },
+    });
+    return;
+  }
+  async findByLogin(login: string): Promise<Client> {
+    const result = await prisma.tbl_client.findUnique({
+      where: { login },
+    });
+    if (!result) return null;
+    const client = Client.create({
+      name: result.name,
+      login: Email.create({ email: result.login }),
+      phone: Phone.create({
+        ddd: result.phone,
+        ddi: result.phone,
+        number: result.phone,
+      }),
+    });
+    client.setId(result.id);
+    return client;
+  }
+}
