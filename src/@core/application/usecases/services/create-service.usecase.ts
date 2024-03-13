fef35f20f0579c40b8fdf8d3ae04e90bcffe92a6
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:32:32 -0300

    add input validation to service usecases

--- src/@core/application/usecases/services/create-service.usecase.ts
+++ src/@core/application/usecases/services/create-service.usecase.ts
@@ -1,6 +1,8 @@
 import { Service } from '../../../domain/entities/services.entity';
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
 import { IServiceRepository } from '../../../domain/repositories/service.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { TInputServicesDTO, TOutputServicesDTO } from '../../dto/services.dto';
 import { mapOutput } from './util';
@@ -9,9 +11,15 @@ export class CreateServiceUseCase {
   constructor(
     private readonly serviceRepository: IServiceRepository,
     private readonly categoryRepository: ICategoryRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
   ) {}
 
   async execute(data: TInputServicesDTO): Promise<TOutputServicesDTO> {
+    const validated = this.validator.validate(this.schema, data);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
     const category = await this.categoryRepository.findById(data.category_id);
     if (!category) {
       throw new NotFoundException('CATEGORY_NOT_FOUND');
--- src/@core/application/usecases/services/update-service.usecase.ts
+++ src/@core/application/usecases/services/update-service.usecase.ts
@@ -3,17 +3,25 @@ import { ICategoryRepository } from '../../../domain/repositories/category.repos
 import { IServiceRepository } from '../../../domain/repositories/service.repository';
 import { TInputServicesDTO, TOutputServicesDTO } from '../../dto/services.dto';
 import { mapOutput } from './util';
+import { IValidation } from '../../../domain/interfaces/validation.interface';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 
 export class UpdateServiceUsecase {
   constructor(
     private readonly serviceRepository: IServiceRepository,
     private readonly categoryRepository: ICategoryRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
   ) {}
 
   async execute(
     data: TInputServicesDTO,
     id: number,
   ): Promise<TOutputServicesDTO> {
+    const validated = this.validator.validate(this.schema, data);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
     const service = await this.serviceRepository.findById(id);
     if (!service) {
       throw new NotFoundException('SERVICE_NOT_FOUND');
