f46add2dcd4db8dd0e75298b48feed2be676d25e
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:03:55 -0300

    add update client schema

--- src/@core/infra/validation/yup/schemas/cliente.schema.ts
+++ src/@core/infra/validation/yup/schemas/cliente.schema.ts
@@ -1,5 +1,8 @@
 import * as yup from 'yup';
-import { TInputClientDTO } from '../../../../application/dto/client.dto';
+import {
+  TInputClientDTO,
+  TInputUpdateClientDTO,
+} from '../../../../application/dto/client.dto';
 
 export const clienteSchema: yup.SchemaOf<TInputClientDTO> = yup.object().shape({
   login: yup.object().shape({ email: yup.string().email().required() }),
@@ -11,3 +14,14 @@ export const clienteSchema: yup.SchemaOf<TInputClientDTO> = yup.object().shape({
     ddi: yup.string().required(),
   }),
 });
+
+export const updateClientSchema: yup.SchemaOf<TInputUpdateClientDTO> = yup
+  .object()
+  .shape({
+    name: yup.string().required(),
+    phone: yup.object().shape({
+      ddd: yup.string().required(),
+      number: yup.string().required(),
+      ddi: yup.string().required(),
+    }),
+  });
