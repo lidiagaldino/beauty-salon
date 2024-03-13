02fcc44901b9ad4c7d5596cad9fe4e86dcbba3d9
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:23:19 -0300

    add input validation to category usecases

--- src/@core/application/usecases/category/create-category.usecase.ts
+++ src/@core/application/usecases/category/create-category.usecase.ts
@@ -1,11 +1,21 @@
 import { Category } from '../../../domain/entities/category.entity';
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { TInputCategoryDTO, TOutputCategoryDTO } from '../../dto/category.dto';
 
 export class CreateCategoryUsecase {
-  constructor(private readonly categoryRepository: ICategoryRepository) {}
+  constructor(
+    private readonly categoryRepository: ICategoryRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
+  ) {}
 
   async execute(input: TInputCategoryDTO): Promise<TOutputCategoryDTO> {
+    const validation = this.validator.validate(this.schema, input);
+    if (!validation.isValid) {
+      throw new BadRequestException(validation.errorsResult);
+    }
     const category = Category.create(input);
     const categoryCreated = await this.categoryRepository.create(category);
     return categoryCreated.toJSON();
--- src/@core/application/usecases/category/update-category.usecase.ts
+++ src/@core/application/usecases/category/update-category.usecase.ts
@@ -1,14 +1,24 @@
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { TInputCategoryDTO, TOutputCategoryDTO } from '../../dto/category.dto';
 
 export class UpdateCategoryUsecase {
-  constructor(private categoryRepository: ICategoryRepository) {}
+  constructor(
+    private readonly categoryRepository: ICategoryRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
+  ) {}
 
   async execute(
     id: number,
     input: TInputCategoryDTO,
   ): Promise<TOutputCategoryDTO> {
+    const validation = this.validator.validate(this.schema, input);
+    if (!validation.isValid) {
+      throw new BadRequestException(validation.errorsResult);
+    }
     const categoryExists = await this.categoryRepository.findById(id);
     if (!categoryExists) {
       throw new NotFoundException('CATEGORY_NOT_FOUND');
