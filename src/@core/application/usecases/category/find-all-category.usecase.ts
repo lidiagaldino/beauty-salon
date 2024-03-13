c2c7d38727c1741e54bebc88912cf500fb86c865
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:52:52 -0300

    add error handling if no items are found to category usecases

--- src/@core/application/usecases/category/find-all-category.usecase.ts
+++ src/@core/application/usecases/category/find-all-category.usecase.ts
@@ -1,10 +1,12 @@
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
+import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { TOutputCategoryDTO } from '../../dto/category.dto';
 
 export class FindAllCategoryUsecase {
   constructor(private readonly categoryRepository: ICategoryRepository) {}
   async execute(): Promise<TOutputCategoryDTO[]> {
     const result = await this.categoryRepository.findAll();
+    if (!result) throw new NotFoundException('CATEGORY_NOT_FOUND');
     return result.map((item) => item.toJSON());
   }
 }
--- src/@core/application/usecases/category/find-category-by-id.usecase.ts
+++ src/@core/application/usecases/category/find-category-by-id.usecase.ts
@@ -1,4 +1,5 @@
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
+import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { TOutputCategoryDTO } from '../../dto/category.dto';
 
 export class FindCategoryByIdUsecase {
@@ -6,6 +7,7 @@ export class FindCategoryByIdUsecase {
 
   async execute(id: number): Promise<TOutputCategoryDTO> {
     const category = await this.categoryRepository.findById(id);
+    if (!category) throw new NotFoundException('CATEGORY_NOT_FOUND');
     return category.toJSON();
   }
 }
