1eda8baf3c9de3bc7c1722eb4a7d6b1577ec761a
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:27:53 -0300

    add input validation to professional usecases

--- src/@core/application/usecases/professional/create-professional.usecase.ts
+++ src/@core/application/usecases/professional/create-professional.usecase.ts
@@ -1,6 +1,8 @@
 import { Professional } from '../../../domain/entities/professional.entity';
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
 import { IProfessionalRepository } from '../../../domain/repositories/professional.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { Email } from '../../../domain/value-objects/email.value-object';
 import { Phone } from '../../../domain/value-objects/phone.value-object';
 import {
@@ -13,9 +15,16 @@ export class CreateProfessionalUsecase {
   constructor(
     private readonly professionalRepository: IProfessionalRepository,
     private readonly categoryRepository: ICategoryRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
   ) {}
 
   async execute(props: TInputProfessionalDTO): Promise<TOutputProfessionalDTO> {
+    const validated = this.validator.validate(this.schema, props);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
+
     const phone = Phone.create(props.phone);
     const email = Email.create(props.email);
     const categories = await Promise.all(
--- src/@core/application/usecases/professional/update-professional.usecase.ts
+++ src/@core/application/usecases/professional/update-professional.usecase.ts
@@ -1,5 +1,7 @@
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { ICategoryRepository } from '../../../domain/repositories/category.repository';
 import { IProfessionalRepository } from '../../../domain/repositories/professional.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { Email } from '../../../domain/value-objects/email.value-object';
 import { Phone } from '../../../domain/value-objects/phone.value-object';
@@ -13,12 +15,19 @@ export class UpdateProfessionalUsecase {
   constructor(
     private readonly professionalRepository: IProfessionalRepository,
     private readonly categoryRepository: ICategoryRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
   ) {}
 
   async execute(
     id: number,
     props: TInputProfessionalDTO,
   ): Promise<TOutputProfessionalDTO> {
+    const validated = this.validator.validate(this.schema, props);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
+
     const professionalExists = await this.professionalRepository.findById(id);
     if (!professionalExists) {
       throw new NotFoundException('PROFESSIONAL_NOT_FOUND');
