2dafc9b523af07eacb6052cb66ee96f499f1fd3e
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:25:53 -0300

    add input validation to cliente usecases

--- src/@core/application/usecases/client/create-client.usecase.ts
+++ src/@core/application/usecases/client/create-client.usecase.ts
@@ -1,5 +1,6 @@
 import { Client } from '../../../domain/entities/client.entity';
 import { IPasswordCryptography } from '../../../domain/interfaces/password.criptography.interface';
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { IClientRepository } from '../../../domain/repositories/client.repository';
 import { Email } from '../../../domain/value-objects/email.value-object';
 import { Phone } from '../../../domain/value-objects/phone.value-object';
@@ -10,9 +11,16 @@ export class CreateClientUsecase {
   constructor(
     private readonly clientRepository: IClientRepository,
     private readonly passwordCryptography: IPasswordCryptography,
+    private readonly validator: IValidation,
+    private readonly schema: object,
   ) {}
 
   async execute(input: TInputClientDTO): Promise<TOutputClientDTO> {
+    const validated = this.validator.validate(this.schema, input);
+    if (!validated.isValid) {
+      throw new Error(validated.errorsResult);
+    }
+
     const password = await this.passwordCryptography.hash(input.password);
     const phone = Phone.create(input.phone);
     const login = Email.create(input.login);
--- src/@core/application/usecases/client/update-client.usecase.ts
+++ src/@core/application/usecases/client/update-client.usecase.ts
@@ -1,16 +1,27 @@
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { IClientRepository } from '../../../domain/repositories/client.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { Phone } from '../../../domain/value-objects/phone.value-object';
 import { TInputUpdateClientDTO, TOutputClientDTO } from '../../dto/client.dto';
 import { mapOutput } from './util';
 
 export class UpdateClientUsecase {
-  constructor(private readonly clientRepository: IClientRepository) {}
+  constructor(
+    private readonly clientRepository: IClientRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
+  ) {}
 
   async execute(
     id: number,
     input: TInputUpdateClientDTO,
   ): Promise<TOutputClientDTO> {
+    const validated = this.validator.validate(this.schema, input);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
+
     const clientExists = await this.clientRepository.findById(id);
     if (!clientExists) {
       throw new NotFoundException('CLIENT_NOT_FOUND');
