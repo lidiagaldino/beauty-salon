a0bd29ca25360a37f19b0e9c210f3fa83193c2f5
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-13 14:34:21 -0300

    add input validation to status usecases

--- src/@core/application/usecases/status/create-status.usecase.ts
+++ src/@core/application/usecases/status/create-status.usecase.ts
@@ -1,11 +1,21 @@
 import { Status } from '../../../domain/entities/status.entity';
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { IStatusRepository } from '../../../domain/repositories/status.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { TInputStatusDTO, TOutputStatusDTO } from '../../dto/status.dto';
 
 export class CreateStatusUsecase {
-  constructor(private readonly statusRepository: IStatusRepository) {}
+  constructor(
+    private readonly statusRepository: IStatusRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
+  ) {}
 
   async execute(data: TInputStatusDTO): Promise<TOutputStatusDTO> {
+    const validated = this.validator.validate(this.schema, data);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
     const status = Status.create(data);
     const result = await this.statusRepository.create(status);
     return result.toJSON();
--- src/@core/application/usecases/status/update-status.usecase.ts
+++ src/@core/application/usecases/status/update-status.usecase.ts
@@ -1,11 +1,21 @@
+import { IValidation } from '../../../domain/interfaces/validation.interface';
 import { IStatusRepository } from '../../../domain/repositories/status.repository';
+import { BadRequestException } from '../../../domain/shared/errors/bad-request.exception';
 import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { TInputStatusDTO, TOutputStatusDTO } from '../../dto/status.dto';
 
 export class UpdateStatusUsecase {
-  constructor(private readonly statusRepository: IStatusRepository) {}
+  constructor(
+    private readonly statusRepository: IStatusRepository,
+    private readonly validator: IValidation,
+    private readonly schema: object,
+  ) {}
 
   async execute(id: number, input: TInputStatusDTO): Promise<TOutputStatusDTO> {
+    const validated = this.validator.validate(this.schema, input);
+    if (!validated.isValid) {
+      throw new BadRequestException(validated.errorsResult);
+    }
     const statusExists = await this.statusRepository.findById(id);
 
     if (!statusExists) {
