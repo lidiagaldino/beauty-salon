23c2f51c16e9223b07ceed46366f68577a8d7b67
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-16 09:45:04 -0300

    fix status controller unit test

--- src/modules/status/status.controller.spec.ts
+++ src/modules/status/status.controller.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StatusController } from './status.controller';
-import { StatusService } from './status.service';
+import { StatusPrismaRepository } from '../../@core/infra/db/prisma/repositories/status.prisma-repository';
+import { CreateStatusUsecase } from '../../@core/application/usecases/status/create-status.usecase';
+import { DeleteStatusUsecase } from '../../@core/application/usecases/status/delete-status.usecase';
+import { FindAllStatusUsecase } from '../../@core/application/usecases/status/find-all-status.usecase';
+import { FindStatusByIdUsecase } from '../../@core/application/usecases/status/find-status-by-id.usecase';
+import { UpdateStatusUsecase } from '../../@core/application/usecases/status/update-status.usecase';
+import { statusSchema } from '../../@core/infra/validation/yup/schemas/status.schema';
+import { YupAdapter } from '../../@core/infra/validation/yup/yup.adapter';
 
 describe('StatusController', () => {
   let controller: StatusController;
@@ -8,7 +15,65 @@ describe('StatusController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StatusController],
-      providers: [StatusService],
+      providers: [
+        {
+          provide: StatusPrismaRepository,
+          useClass: StatusPrismaRepository,
+        },
+        {
+          provide: YupAdapter,
+          useClass: YupAdapter,
+        },
+        {
+          provide: CreateStatusUsecase,
+          useFactory: (
+            statusPrismaRepository: StatusPrismaRepository,
+            yupAdapter: YupAdapter,
+          ) => {
+            return new CreateStatusUsecase(
+              statusPrismaRepository,
+              yupAdapter,
+              statusSchema,
+            );
+          },
+          inject: [StatusPrismaRepository, YupAdapter],
+        },
+        {
+          provide: UpdateStatusUsecase,
+          useFactory: (
+            statusPrismaRepository: StatusPrismaRepository,
+            yupAdapter: YupAdapter,
+          ) => {
+            return new UpdateStatusUsecase(
+              statusPrismaRepository,
+              yupAdapter,
+              statusSchema,
+            );
+          },
+          inject: [StatusPrismaRepository, YupAdapter],
+        },
+        {
+          provide: DeleteStatusUsecase,
+          useFactory: (statusPrismaRepository: StatusPrismaRepository) => {
+            return new DeleteStatusUsecase(statusPrismaRepository);
+          },
+          inject: [StatusPrismaRepository],
+        },
+        {
+          provide: FindAllStatusUsecase,
+          useFactory: (statusPrismaRepository: StatusPrismaRepository) => {
+            return new FindAllStatusUsecase(statusPrismaRepository);
+          },
+          inject: [StatusPrismaRepository],
+        },
+        {
+          provide: FindStatusByIdUsecase,
+          useFactory: (statusPrismaRepository: StatusPrismaRepository) => {
+            return new FindStatusByIdUsecase(statusPrismaRepository);
+          },
+          inject: [StatusPrismaRepository],
+        },
+      ],
     }).compile();
 
     controller = module.get<StatusController>(StatusController);
