8e1661cc163fce2a1c1d5b2229be458497b0dd62
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-18 14:18:49 -0300

    add map output method to ensure srp

--- src/@core/infra/db/prisma/repositories/category.prisma-repository.ts
+++ src/@core/infra/db/prisma/repositories/category.prisma-repository.ts
@@ -9,41 +9,38 @@ export class CategoryPrismaRepository implements ICategoryRepository {
         name: category.getName(),
       },
     });
-    const createdCategory = Category.create({ name: result.name });
-    createdCategory.setId(result.id);
-    return createdCategory;
+    category.setId(result.id);
+    return category;
   }
   async findAll(): Promise<Category[]> {
     const result = await prisma.tbl_category.findMany();
-    const categories = result.map((item) => {
-      const category = Category.create({ name: item.name });
-      category.setId(item.id);
-      return category;
-    });
-    return categories.length > 0 ? categories : null;
+    if(result.length == 0) return null;
+    return result.map(this.mapOutput);
   }
   async findById(id: number): Promise<Category> {
     const result = await prisma.tbl_category.findUnique({
       where: { id },
     });
     if (!result) return null;
-    const category = Category.create({ name: result.name });
-    category.setId(result.id);
-    return category;
+    return this.mapOutput(result);
   }
   async update(category: Category): Promise<Category> {
-    const result = await prisma.tbl_category.update({
+    await prisma.tbl_category.update({
       where: { id: category.getId() },
       data: { name: category.getName() },
     });
-    const updatedCategory = Category.create({ name: result.name });
-    updatedCategory.setId(result.id);
-    return updatedCategory;
+    return category;
   }
   async delete(id: number): Promise<void> {
     await prisma.tbl_category.delete({
       where: { id },
     });
     return;
+  }
+
+  private mapOutput(input: {id: number, name: string}): Category {
+    const category = Category.create({ name: input.name });
+    category.setId(input.id);
+    return category;
   }
 }
